19edc37db3aac41fd085e2320b8d90d77f1020f1
Vaishali Bhaip <vaishali.bhaip@tatacommunications.com>
2019-06-17 22:51:54 +0530

    addednewapi

--- src/main/java/com/tatacommunications/vtt/controller/TranscriptController.java
+++ src/main/java/com/tatacommunications/vtt/controller/TranscriptController.java
@@ -1,9 +1,17 @@
 package com.tatacommunications.vtt.controller;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -50,11 +58,49 @@ public class TranscriptController {
     
     
     @CrossOrigin(origins = "*")
-   	@RequestMapping(value = "/get", method = RequestMethod.GET)
-   	public String get() {
-    	System.out.println("Helooooo");
+   	@RequestMapping(value = "/get", method = RequestMethod.POST)
+   	public String get(@RequestParam("url") String url,@RequestParam("name") String name) {
+    	try {
+			downloadUsingStream(url,name);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
    		return new Gson().toJson("Helooo");
    	}
+    
+    
+	  private static void downloadUsingStream(String urlStr, String file) throws IOException{
+	        URL url = new URL(urlStr);
+	        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+	        FileOutputStream fis = new FileOutputStream(file);
+	        byte[] buffer = new byte[1024];
+	        int count=0;
+	        while((count = bis.read(buffer,0,1024)) != -1)
+	        {
+	        	
+	            fis.write(buffer, 0, count);
+	        }
+	        fis.close();
+	        bis.close();
+	        try {
+	        	 FileInputStream new1=new FileInputStream(file);
+	        }catch(Exception e) {
+	        	System.out.println("File Not Found");
+	        	e.printStackTrace();
+	        }
+	       
+	    }
+
+	    private static void downloadUsingNIO(String urlStr, String file) throws IOException {
+	        URL url = new URL(urlStr);
+	        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+	        FileOutputStream fos = new FileOutputStream(file);
+	        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+	        fos.close();
+	        rbc.close();
+	    }
+
 
 
 }
