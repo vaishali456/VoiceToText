b43567035f442039b6242eeb0fd5f5ebb350d2ca
Vaishali Bhaip <vaishali.bhaip@tatacommunications.com>
2019-06-18 12:26:57 +0530

    added change

--- src/main/java/com/tatacommunications/vtt/ibmApiCaller/apiCaller.java
+++ src/main/java/com/tatacommunications/vtt/ibmApiCaller/apiCaller.java
@@ -1,11 +1,19 @@
 package com.tatacommunications.vtt.ibmApiCaller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Arrays;
 
 import org.springframework.stereotype.Component;
 
+import com.ibm.cloud.sdk.core.http.HttpMediaType;
 import com.ibm.cloud.sdk.core.service.security.IamOptions;
 import com.ibm.watson.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
@@ -16,18 +24,21 @@ import com.ibm.watson.speech_to_text.v1.websocket.BaseRecognizeCallback;
 public class apiCaller {
 	String speechRecognitionResultsInString=null;
 	boolean flag=false;
+	int cnt=1;
 
 	public String getSpeechRecognitionResults(String filepath, String model, String contentType) {
 			System.out.println("getSpeechRecognitionResults");
-		//APICaller
-				IamOptions options = new IamOptions.Builder()
+		//APICalle
+			
+				  IamOptions options2 = new IamOptions.Builder()
 		    		    .apiKey("NW2RvdRP8ItssRduLU6QwpauKtmbHEPouP97iYF2iMC7")
 		    		    .build();
 
-		    		SpeechToText speechToText = new SpeechToText(options);
+		    		SpeechToText speechToText = new SpeechToText(options2);
 
-		    		speechToText.setEndPoint("https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize");
+		    		speechToText.setEndPoint("https://gateway-lon.watsonplatform.net/speech-to-text/api");
 
+		    	
 		    		try {
 		    		  RecognizeOptions recognizeOptions = new RecognizeOptions.Builder()
 		    		    .audio(new FileInputStream(filepath))
@@ -37,6 +48,9 @@ public class apiCaller {
 		    		    .keywordsThreshold((float) 0.5)
 		    		    .maxAlternatives(3)
 		    		    .build();
+		    		  
+		    		  /*SpeechRecognitionResults transcript1 = speechToText.recognize(recognizeOptions).execute().getResult();
+		    		  System.out.println(transcript1); */
 		    		  
 		    		  BaseRecognizeCallback baseRecognizeCallback =
 		    		    new BaseRecognizeCallback() {
@@ -50,13 +64,17 @@ public class apiCaller {
 		    		      @Override
 		    		      public void onDisconnected() {
 		    		    	  flag=true;
+		    		    	  cnt++;
 		    		      //  System.exit(0);
 		    		      }
 		    		    };
 		    	
 		    		 speechToText.recognizeUsingWebSocket(recognizeOptions,
 		    		    baseRecognizeCallback);
-			while (flag == false) {
+		    	 if(cnt > 3) {
+		 				return null;
+		 			}
+			while (flag == false && cnt < 3) {
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -64,14 +82,18 @@ public class apiCaller {
 					e.printStackTrace();
 				}
 			}
+			
 
-		} catch (FileNotFoundException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}  
 		    		
 			
     		
 		return speechRecognitionResultsInString;
 
 	}
+	
+	
+
 }
